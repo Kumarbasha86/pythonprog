acef76dc20ea3b22b69740f67dcf255fd4752b42
kumar basha <kumarbasha908@gmail.com>
2026-05-22 17:15:20 +0530

    this is my first prog

---
+++ hello.py
@@ -0,0 +1,15 @@
+# hello program
+print("hello world")
+
+# sum of two  numbers 
+a=10 
+b=23
+c=a+b
+print(c)
+
+# input taken from user 
+a=int(input("enter a number "))
+b=int(input("enter the b number "))
+c=a+b
+
+print(c)
